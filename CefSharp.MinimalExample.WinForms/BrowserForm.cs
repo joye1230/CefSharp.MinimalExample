703069219b8468f80729b7e60365a0f0565920ce
DESKTOP-1U2B0NT\joye <zhangzhizhuo@hotmail.com>
2016-10-07 13:27:59 +0800

    第一版

--- CefSharp.MinimalExample.WinForms/BrowserForm.cs
+++ CefSharp.MinimalExample.WinForms/BrowserForm.cs
@@ -12,135 +12,25 @@ namespace CefSharp.MinimalExample.WinForms
     public partial class BrowserForm : Form
     {
         private readonly ChromiumWebBrowser browser;
-
+        private Sunisoft.IrisSkin.SkinEngine skinEngine1;
         public BrowserForm()
         {
             InitializeComponent();
+            this.skinEngine1 = new Sunisoft.IrisSkin.SkinEngine(((System.ComponentModel.Component)(this)));
+            this.skinEngine1.SkinFile = Application.StartupPath + "//Skins//wolf_kill.ssk";
+            Text = "狼人传说";
+            //WindowState = FormWindowState.Maximized;
 
-            Text = "CefSharp";
-            WindowState = FormWindowState.Maximized;
-
-            browser = new ChromiumWebBrowser("www.google.com")
+           
+            browser = new ChromiumWebBrowser("http://121.40.64.66:8081/Web/login.php")
             {
                 Dock = DockStyle.Fill,
+                
             };
             toolStripContainer.ContentPanel.Controls.Add(browser);
-
-            browser.LoadingStateChanged += OnLoadingStateChanged;
-            browser.ConsoleMessage += OnBrowserConsoleMessage;
-            browser.StatusMessage += OnBrowserStatusMessage;
-            browser.TitleChanged += OnBrowserTitleChanged;
-            browser.AddressChanged += OnBrowserAddressChanged;
-
-            var bitness = Environment.Is64BitProcess ? "x64" : "x86";
-            var version = String.Format("Chromium: {0}, CEF: {1}, CefSharp: {2}, Environment: {3}", Cef.ChromiumVersion, Cef.CefVersion, Cef.CefSharpVersion, bitness);
-            DisplayOutput(version);
         }
 
-        private void OnBrowserConsoleMessage(object sender, ConsoleMessageEventArgs args)
-        {
-            DisplayOutput(string.Format("Line: {0}, Source: {1}, Message: {2}", args.Line, args.Source, args.Message));
-        }
 
-        private void OnBrowserStatusMessage(object sender, StatusMessageEventArgs args)
-        {
-            this.InvokeOnUiThreadIfRequired(() => statusLabel.Text = args.Value);
-        }
-
-        private void OnLoadingStateChanged(object sender, LoadingStateChangedEventArgs args)
-        {
-            SetCanGoBack(args.CanGoBack);
-            SetCanGoForward(args.CanGoForward);
-
-            this.InvokeOnUiThreadIfRequired(() => SetIsLoading(!args.CanReload));
-        }
-
-        private void OnBrowserTitleChanged(object sender, TitleChangedEventArgs args)
-        {
-            this.InvokeOnUiThreadIfRequired(() => Text = args.Title);
-        }
-
-        private void OnBrowserAddressChanged(object sender, AddressChangedEventArgs args)
-        {
-            this.InvokeOnUiThreadIfRequired(() => urlTextBox.Text = args.Address);
-        }
-
-        private void SetCanGoBack(bool canGoBack)
-        {
-            this.InvokeOnUiThreadIfRequired(() => backButton.Enabled = canGoBack);
-        }
-
-        private void SetCanGoForward(bool canGoForward)
-        {
-            this.InvokeOnUiThreadIfRequired(() => forwardButton.Enabled = canGoForward);
-        }
-
-        private void SetIsLoading(bool isLoading)
-        {
-            goButton.Text = isLoading ?
-                "Stop" :
-                "Go";
-            goButton.Image = isLoading ?
-                Properties.Resources.nav_plain_red :
-                Properties.Resources.nav_plain_green;
-
-            HandleToolStripLayout();
-        }
-
-        public void DisplayOutput(string output)
-        {
-            this.InvokeOnUiThreadIfRequired(() => outputLabel.Text = output);
-        }
-
-        private void HandleToolStripLayout(object sender, LayoutEventArgs e)
-        {
-            HandleToolStripLayout();
-        }
-
-        private void HandleToolStripLayout()
-        {
-            var width = toolStrip1.Width;
-            foreach (ToolStripItem item in toolStrip1.Items)
-            {
-                if (item != urlTextBox)
-                {
-                    width -= item.Width - item.Margin.Horizontal;
-                }
-            }
-            urlTextBox.Width = Math.Max(0, width - urlTextBox.Margin.Horizontal - 18);
-        }
-
-        private void ExitMenuItemClick(object sender, EventArgs e)
-        {
-            browser.Dispose();
-            Cef.Shutdown();
-            Close();
-        }
-
-        private void GoButtonClick(object sender, EventArgs e)
-        {
-            LoadUrl(urlTextBox.Text);
-        }
-
-        private void BackButtonClick(object sender, EventArgs e)
-        {
-            browser.Back();
-        }
-
-        private void ForwardButtonClick(object sender, EventArgs e)
-        {
-            browser.Forward();
-        }
-
-        private void UrlTextBoxKeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode != Keys.Enter)
-            {
-                return;
-            }
-
-            LoadUrl(urlTextBox.Text);
-        }
 
         private void LoadUrl(string url)
         {
@@ -148,6 +38,11 @@ namespace CefSharp.MinimalExample.WinForms
             {
                 browser.Load(url);
             }
+        }
+
+        private void BrowserForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,12 +1,25 @@
-CefSharp.MinimalExample
+这是趣蛙网络的狼人杀项目，使用了CefSharp框架作为浏览器 charom47版本
 =======================
 
-Minimal example of how the CefSharp library can be used using the official `Nuget` packages. 
 
-Includes examples for
-- WinForms
-- WPF
-- OffScreen
- 
 
-For a more complete example of each project see the main `CefSharp` repository.
+WinForms
+这里放了客户端主程序，打开后直接编译即可
+皮肤文件为 wolf_kill.ssk，使用IrisSkin4.dll调用的
+需要编辑皮肤文件，请使用tools里面的irisskin_setup_chs.zip对皮肤进行编辑
+
+打包
+wolfkill为打包使用目录
+根据VS的版本，以下目录可能会有不同请选择当前VS的目录
+1.打包fameworlk
+fameworlk
+tools/fameworlk4.0文件放入到
+C:\Program Files (x86)\Microsoft Visual Studio 14.0\SDK\Bootstrapper\Packages\DotNetFX40
+fameworlk中文语言包
+tools/dotNetFx40LP_Full_x86_x64zh-Hans.exe
+C:\Program Files (x86)\Microsoft Visual Studio 14.0\SDK\Bootstrapper\Packages\DotNetFX40\zh-Hans
+
+2.打包VC20014++库
+如果VS已经有VC2013++选项就不用生成，如果没有覆盖上VS默认的
+tools/vcredist_x86目录覆盖
+C:\Program Files (x86)\Microsoft Visual Studio 14.0\SDK\Bootstrapper\Packages
